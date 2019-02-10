1df64828c4119d729350262b5e668fed57471c8f
Weston Sleeman <westonsleeman@hotmail.com>
2019-02-10 09:05:25 +0000

    Updated string handling of Thread()

--- Runtime/Util/Util.vb
+++ Runtime/Util/Util.vb
@@ -10,7 +10,7 @@
 	Public Function Thread(ParamArray Data() As Object) As Object
 		Dim Separator = Data(0)
 		Data = Data.Skip(1).ToArray()
-		If Data.Length = 1 AndAlso TypeOf Data(0) Is IEnumerable(Of Object) AndAlso TypeOf Data(0) IsNot String Then Return ThreadLocal(Separator, Data(0))
+		If Data.Length = 1 AndAlso TypeOf Data(0) Is IEnumerable(Of Object) Then Return ThreadLocal(Separator, If(TypeOf Data(0) Is String, Data(0).ToString().ToCharArray(), Data(0)))
 		Return Data.Aggregate(Function(threader, item) threader & Separator & If(TypeOf item Is IEnumerable(Of Object) AndAlso TypeOf item IsNot String, ThreadLocal(Separator, item), item))
 	End Function
 
