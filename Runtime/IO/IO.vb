6d27ce2a4a74f38d7bfaecb5fe7e1270dc9b5fa8
Weston Sleeman <westonsleeman@hotmail.com>
2019-02-11 16:29:50 +0000

    Updated Type function in standard library to handle multi-level nested data structures.

--- Runtime/IO/IO.vb
+++ Runtime/IO/IO.vb
@@ -1,24 +1,26 @@
 Imports System.IO
 
 Public Module IO
-	Public Sub Type(ParamArray Data() As Object)
-		Dim Sterilise = Function(inp$)
-							Return inp.Replace("\\", "ʍbɐcĸßℓɐßɥ").Replace("\r\n", vbCrLf).Replace("\r", vbCr).Replace("\n", Environment.NewLine).Replace("\b", vbBack).Replace("\t", vbTab).Replace("ʍbɐcĸßℓɐßɥ", "\")
-						End Function
+    Public Sub Type(ParamArray Data() As Object)
+        TypeLocal(Data)
+        Console.WriteLine()
+    End Sub
 
-		For Each item In Data
-			If (Not TypeOf item Is String) AndAlso TypeOf item Is IEnumerable Then
-				For Each index In item
-					Console.Write(Sterilise(index))
-				Next
-			Else
-				Console.Write(Sterilise(item))
-			End If
-		Next
-		Console.WriteLine()
-	End Sub
+    Private Sub TypeLocal(Data() As Object)
+        Dim Sterilise = Function(inp$)
+                            Return inp.Replace("\\", "ʍbɐcĸßℓɐßɥ").Replace("\r\n", vbCrLf).Replace("\r", vbCr).Replace("\n", Environment.NewLine).Replace("\b", vbBack).Replace("\t", vbTab).Replace("ʍbɐcĸßℓɐßɥ", "\")
+                        End Function
 
-	Public Function Read() As Object
+        For Each item In Data
+            If (Not TypeOf item Is String) AndAlso TypeOf item Is IEnumerable Then
+                TypeLocal(CType(item, IEnumerable(Of Object)).ToArray())
+            Else
+                Console.Write(Sterilise(item))
+            End If
+        Next
+    End Sub
+
+    Public Function Read() As Object
 		Return Console.ReadLine()
 	End Function
 
